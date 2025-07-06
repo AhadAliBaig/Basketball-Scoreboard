999737193f62f1295afe2a38a1b5ea7c582b1a9c
Ahad Baig <ahadalibaig689@gmail.com>
2025-07-06 19:44:31 +0500

    added buttons and functionality with js

---
+++ index.js
@@ -0,0 +1,11 @@
+
+let homeScoreEl = document.getElementById('home-score')
+let guestScoreEl = document.getElementById('guest-score')
+let homeScore = 0
+let guestScore = 0
+
+function score1 (){
+    homeScoreEl += 1
+    
+
+}
