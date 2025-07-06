ee57d62dcf261ce5d65e971eea2bf6123f3476b9
Ahad Baig <ahadalibaig689@gmail.com>
2025-07-06 19:55:15 +0500

    fixed all the errors

--- index.js
+++ index.js
@@ -1,11 +1,55 @@
 
-let homeScoreEl = document.getElementById('home-score')
-let guestScoreEl = document.getElementById('guest-score')
+let homeScoreEl = document.getElementById("home-score-el")
+let guestScoreEl = document.getElementById('guest-score-el')
+let resetBtnEl = document.getElementById('reset-btn')
 let homeScore = 0
 let guestScore = 0
 
-function score1 (){
-    homeScoreEl += 1
-    
 
+//for home team
+function score1() {
+    homeScore += 1
+    homeScoreEl.textContent = homeScore
+
+}
+
+function score2() {
+    homeScore += 2
+    homeScoreEl.textContent = homeScore
+
+}
+
+function score3() {
+    homeScore += 3
+    homeScoreEl.textContent = homeScore
+
+}
+
+//for guest team
+
+function gscore1() {
+    guestScore += 1
+    guestScoreEl.textContent = guestScore
+
+}
+
+function gscore2() {
+    guestScore += 2
+    guestScoreEl.textContent = guestScore
+
+}
+
+function gscore3() {
+    guestScore += 3
+    guestScoreEl.textContent = guestScore
+
+}
+
+//reset button
+
+function reset() {
+    homeScore = 0
+    guestScore = 0
+    homeScoreEl.textContent = homeScore
+    guestScoreEl.textContent = guestScore
 }
